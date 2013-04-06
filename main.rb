96a59e60e679ec804bdd68316d79c0bc62a42aa0
Nurettin Onur TUĞCU <onurtugcu@gmail.com>
2013-04-06 17:48:41 +0300

    kod duzenle

--- main.rb
+++ main.rb
@@ -11,12 +11,22 @@ java_import 'net.zemberek.tr.yapi.TurkiyeTurkcesi'
 
 z= Zemberek.new(TurkiyeTurkcesi.new)
 
-metin= "ne yapsam aşktan kaçamıyorum apple"
+metin= "ne yapsam aşktan kaçamıyorum yağmurdan kaçabildiğim kador apple"
 
-dizi= YaziIsleyici.analizDizisiOlustur(metin).reject{ |kelime| kelime.icerik.strip.empty? }
-cozumler= dizi.map{ |kelime| [kelime.icerik, z.kelimeDenetle(kelime.icerik) ? "Türkçe": z.oner(kelime.icerik).first] 
-#z.kelimeCozumle(kelime.icerik) 
-}
+dizi= YaziIsleyici.analizDizisiOlustur(metin).
+	reject{ |kelime| kelime.icerik.strip.empty? }
+
+cozumler= dizi.map do |kelime| 
+	[
+		kelime.icerik, 
+		z.kelimeCozumle(kelime.icerik),
+		if z.kelimeDenetle(kelime.icerik) 
+			"Türkçe"
+		else
+			z.oner(kelime.icerik).first
+		end
+	] 
+end
 
 p metin
 p cozumler
